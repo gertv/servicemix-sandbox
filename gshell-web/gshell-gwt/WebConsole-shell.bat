2f44bb6c61de35b41548f40ae10287140ba5cfa3
kkoehler <kkoehler@13f79535-47bb-0310-9956-ffa450edef68>
2008-07-04 13:05:42 +0000

    * initial import of windows batch files to compile and start the webconsole. * added www, tomcat and target directories to ignore list
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/sandbox@674038 13f79535-47bb-0310-9956-ffa450edef68

---
+++ gshell-web/gshell-gwt/WebConsole-shell.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set CURRENT_DIR=%~dp0
+set PLATFORM=windows
+set GWT_HOME_DIR=D:\opensource-projects\gwt-windows-1.5.0
+
+if not exist "%GWT_HOME_DIR%" goto gwt_home
+
+@echo on 
+java -Xmx256M -cp "%CURRENT_DIR%\src;%CURRENT_DIR%\bin;%CURRENT_DIR%\target\classes;%GWT_HOME_DIR%\gwt-user.jar;%GWT_HOME_DIR%\gwt-dev-%PLATFORM%.jar" com.google.gwt.dev.GWTShell -out "%CURRENT_DIR%\www" "$@" org.apache.servicemix.gshellweb.WebConsole\WebConsole.html
+
+@echo off
+goto end
+
+:gwt_home
+echo ==================================
+echo GWT_HOME_DIR not set correctly!
+echo please set within this batch file
+echo ==================================
+
+:end
